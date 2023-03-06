12f3a1cef6759e95cc454806daac255a8b59078e
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-03-06 14:20:03 +0530

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/layouts/admin-layout/admin-layout.component.css 	new file:   src/app/layouts/admin-layout/admin-layout.component.html 	new file:   src/app/layouts/admin-layout/admin-layout.component.spec.ts 	new file:   src/app/layouts/admin-layout/admin-layout.component.ts 	new file:   src/app/pages/about/about.component.css 	new file:   src/app/pages/about/about.component.html 	new file:   src/app/pages/about/about.component.spec.ts 	new file:   src/app/pages/about/about.component.ts 	new file:   src/app/pages/contact/contact.component.css 	new file:   src/app/pages/contact/contact.component.html 	new file:   src/app/pages/contact/contact.component.spec.ts 	new file:   src/app/pages/contact/contact.component.ts 	new file:   src/app/pages/home/home.component.css 	new file:   src/app/pages/home/home.component.html 	new file:   src/app/pages/home/home.component.spec.ts 	new file:   src/app/pages/home/home.component.ts 	new file:   src/app/pages/services/services.component.css 	new file:   src/app/pages/services/services.component.html 	new file:   src/app/pages/services/services.component.spec.ts 	new file:   src/app/pages/services/services.component.ts 	new file:   src/app/pages/visas/visas.component.css 	new file:   src/app/pages/visas/visas.component.html 	new file:   src/app/pages/visas/visas.component.spec.ts 	new file:   src/app/pages/visas/visas.component.ts 	new file:   src/app/shared/navbar/navbar.component.css 	new file:   src/app/shared/navbar/navbar.component.html 	new file:   src/app/shared/navbar/navbar.component.spec.ts 	new file:   src/app/shared/navbar/navbar.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { VisasComponent } from './visas/visas.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    AdminLayoutComponent,
+    VisasComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
