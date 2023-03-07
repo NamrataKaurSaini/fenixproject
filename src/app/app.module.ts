eced964d7cb14ae6939d308067eaf8505108525e
Muskanvij <vijmuskan94@gmail.com>
2023-03-07 13:00:55 +0530

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	modified:   src/app/pages/about/about.component.html 	modified:   src/app/pages/contact/contact.component.html 	modified:   src/app/pages/contact/contact.component.ts 	modified:   src/app/pages/home/home.component.html 	modified:   src/app/pages/services/services.component.html 	modified:   src/app/shared/navbar/navbar.component.html 	new file:   src/assets/img/1.png 	new file:   src/assets/img/home-eu.png 	modified:   src/main.ts 	modified:   tsconfig.app.json 	modified:   tsconfig.spec.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { VisasComponent } from './visas/visas.component';
-import { ServicesComponent } from './services/services.component';
+import { VisasComponent } from './pages/visas/visas.component';
+import { ServicesComponent } from './pages/services/services.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { ServicesComponent } from './services/services.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
