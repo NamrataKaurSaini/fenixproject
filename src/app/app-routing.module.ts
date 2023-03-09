9f8a6fcd1bdd83bb813ce6344fc69880796cf0f8
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-03-09 16:25:54 +0530

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	modified:   src/app/pages/about/about.component.html 	modified:   src/app/pages/contact/contact.component.html 	modified:   src/app/pages/contact/contact.component.ts 	modified:   src/app/pages/home/home.component.css 	modified:   src/app/pages/home/home.component.html 	modified:   src/app/pages/services/services.component.css 	modified:   src/app/pages/services/services.component.html 	modified:   src/app/pages/visas/visas.component.html 	new file:   src/app/shared/footer/footer.component.css 	new file:   src/app/shared/footer/footer.component.html 	new file:   src/app/shared/footer/footer.component.spec.ts 	new file:   src/app/shared/footer/footer.component.ts 	modified:   src/app/shared/navbar/navbar.component.html 	new file:   src/assets/img/1.png 	new file:   src/assets/img/3.jpg 	new file:   src/assets/img/4.jpg 	new file:   src/assets/img/Screenshot_2023-03-07_at_4.19.26_PM-removebg-preview.png 	new file:   src/assets/img/Screenshot_2023-03-07_at_4.19.42_PM-removebg-preview.png 	new file:   src/assets/img/Untitled design.jpg 	new file:   src/assets/img/alex-kotliarskyi-QBpZGqEMsKg-unsplash.jpg 	new file:   src/assets/img/fenixLogo.png 	new file:   src/assets/img/fenixLogoWithText.png 	new file:   src/assets/img/fenixLogoWithTextInWhite.png 	new file:   src/assets/img/home-eu.png 	modified:   src/main.ts 	modified:   tsconfig.app.json 	modified:   tsconfig.spec.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { VisasComponent } from './pages/visas/visas.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'visas', component: VisasComponent},
+    {path: 'services', component: ServicesComponent} ,
+    {path: 'contact', component: ContactComponent}
+  ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
