ff0444ba3015269cabcccde62f25dd8b1e6f5763
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-03-13 15:02:32 +0530

    Base UI created

---
+++ fenixproject/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component'; 
+import { HomeComponent } from './pages/home/home.component'; 
+import { ContactComponent } from './pages/contact/contact.component'; 
+import { AboutComponent } from './pages/about/about.component'; 
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { VisasComponent } from './pages/visas/visas.component'; 
+import { ServicesComponent } from './pages/services/services.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    AdminLayoutComponent,
+    VisasComponent,
+    ServicesComponent,
+    FooterComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ fenixproject/src/app/pages/visas/visas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visas',
+  templateUrl: './visas.component.html',
+  styleUrls: ['./visas.component.css']
+})
+export class VisasComponent {
+
+}
