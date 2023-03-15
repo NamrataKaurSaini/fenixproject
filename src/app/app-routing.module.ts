0862f5efcdc17d74cf62dcafc62d97891346f0ef
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-03-15 13:57:59 +0530

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.module.ts 	modified:   src/app/pages/about/about.component.html 	modified:   src/app/pages/contact/contact.component.html 	modified:   src/app/pages/contact/contact.component.ts 	modified:   src/app/pages/home/home.component.css 	modified:   src/app/pages/home/home.component.html 	modified:   src/app/pages/services/services.component.css 	modified:   src/app/pages/services/services.component.html 	modified:   src/app/pages/visas/visas.component.css 	modified:   src/app/pages/visas/visas.component.html 	modified:   src/app/shared/footer/footer.component.html 	modified:   src/app/shared/navbar/navbar.component.html 	new file:   src/assets/img/kerwin-elias-TKE02PRu97k-unsplash.jpg 	modified:   src/index.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { VisasComponent } from './pages/visas/visas.component';
-import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { VisasComponent } from './pages/visas/visas.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'visas', component: VisasComponent},
     {path: 'services', component: ServicesComponent} ,
     {path: 'contact', component: ContactComponent}
-  ];
-
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './shared/navbar/navbar.component'; 
+import { HomeComponent } from './pages/home/home.component'; 
+import { ContactComponent } from './pages/contact/contact.component'; 
+import { AboutComponent } from './pages/about/about.component'; 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { VisasComponent } from './pages/visas/visas.component';
+import { VisasComponent } from './pages/visas/visas.component'; 
 import { ServicesComponent } from './pages/services/services.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -24,12 +25,14 @@ import { FooterComponent } from './shared/footer/footer.component';
     AdminLayoutComponent,
     VisasComponent,
     ServicesComponent,
-    FooterComponent
+    FooterComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
